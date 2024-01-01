13f9b9cee951f969cba10660fcd16dcf476c55d2
William Perez <wperez185@yahoo.com>
2024-01-01 15:00:46 -0500

    added timerchallenge componet for card

---
+++ src/components/TimerChallenge.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function TimerChallenge({title, targetTime}) {
+  return (
+    <section className="challenge">
+        <h2>{title}</h2>
+        <p className="challenge-time">
+            {targetTime} second{targetTime > 1 ? 's' : ''}
+        </p>
+        <p>
+            <button>Start Challenge</button>
+        </p>
+        <p className="active">
+            Time is running... / Timer inactive
+        </p>
+    </section>
+  )
+}
