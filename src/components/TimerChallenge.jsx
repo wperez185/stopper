47d07c0f400f994a49460f3fb5473f2f98cefae6
William Perez <wperez185@yahoo.com>
2024-01-01 16:25:13 -0500

    change settimeout to interval function

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -6,20 +6,26 @@ export default function TimerChallenge({title, targetTime}) {
     const timer = useRef();
     const dialog = useRef();
 
-    const [timerStarted, setTimerStarted] = useState(false);
-    const [timerExpired, setTimerExpired] = useState(false);
+    const [timeRemaining, setTimeRemaining] = useState(targetTime * 1000);
+
+    const timerIsActive = timeRemaining > 0 && timeRemaining < targetTime * 1000;
+
+    if(timeRemaining <= 0) {
+        clearInterval(timer.current);
+        setTimeRemaining(targetTime * 1000);
+        dialog.current.open();
+    }
 
     function handleStart() {
-         timer.current = setTimeout(() => {
-            setTimerExpired(true);
-            dialog.current.open();
-        }, targetTime * 1000);
+         timer.current = setInterval(() => {
+            setTimeRemaining(prevTimeRemaining => prevTimeRemaining - 10);
+        }, 10);
 
-        setTimerStarted(true);
     }
 
     function handleStop() {
-        clearTimeout(timer.current);
+        dialog.current.open();
+        clearInterval(timer.current);
     }
 
   return (
@@ -31,10 +37,10 @@ export default function TimerChallenge({title, targetTime}) {
                 {targetTime} second{targetTime > 1 ? 's' : ''}
             </p>
             <p>
-                <button onClick={timerStarted ? handleStop : handleStart}>{timerStarted ? 'Stop' : 'Start'} Challenge</button>
+                <button onClick={timerIsActive ? handleStop : handleStart}>{timerIsActive ? 'Stop' : 'Start'} Challenge</button>
             </p>
-            <p className={timerStarted ? 'active' : undefined}>
-            {timerStarted ? 'Time is running...' : 'Timer inactive'} 
+            <p className={timerIsActive ? 'active' : undefined}>
+            {timerIsActive ? 'Time is running...' : 'Timer inactive'} 
             </p>
         </section>
     </>
